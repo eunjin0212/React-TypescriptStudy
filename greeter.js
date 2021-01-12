dfa707faa8edef64b58334e59e9315ed2bef645e
eunjin0212 <eunjin9639@gmail.com>
2021-01-12 17:51:19 +0900

    Classes

--- greeter.js
+++ greeter.js
@@ -1,5 +1,14 @@
-function greeter(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + ' ' + lastName;
+    }
+    return Student;
+}());
+function greeter2(person) {
+    return 'Hello, ' + person.firstName + ' ' + person.lastName;
 }
-var user = { firstName: "Kim", lastName: "jin" };
-console.log(greeter(user));
+var user2 = new Student('Kim', 'E.', 'Jin');
+console.log(greeter2(user2));
--- greeter.ts
+++ greeter.ts
@@ -1,12 +1,17 @@
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + ' ' + lastName;
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
 }
 
-function greeter(person: Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function greeter2(person: Person) {
+    return 'Hello, ' + person.firstName + ' ' + person.lastName;
 }
-let user = { firstName: "Kim", lastName: "jin" };
-
-console.log(greeter(user));
-
+let user2 = new Student('Kim', 'E.', 'Jin');
+console.log(greeter2(user2));
